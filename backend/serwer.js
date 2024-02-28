a0b3bf72f0ddf0f4b31e6121af6739f4e587821e
kubashh <146523311+kubashh@users.noreply.github.com>
2024-02-28 15:23:25 +0000

    Dodanie backendu

--- backend/serwer.js
+++ backend/serwer.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  console.log(req);
+  res.send("Serwer działa");
+});
+
+app.listen(8888, () => {
+  console.log("Aplikacja wystartowała na porcie 8888");
+});
